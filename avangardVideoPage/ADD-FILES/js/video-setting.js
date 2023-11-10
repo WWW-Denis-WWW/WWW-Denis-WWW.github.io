8b7047081904529f728a0803758dcb2dc9e4954d
Denis_[WEB]_ <74264830+WWW-Denis-WWW@users.noreply.github.com>
2023-11-10 20:39:39 +0500

    zxc2

--- avangardVideoPage/ADD-FILES/js/video-setting.js
+++ avangardVideoPage/ADD-FILES/js/video-setting.js
@@ -1,6 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const player = videojs('video-player', {
-        fluid: true,
-        fill: true
-    });
-})
+const player = videojs('video-player', {
+    fluid: true,
+    fill: true,
+    handleMouseDown: () => {
+        console.log('zxc')
+    },
+    plugins: {
+        hotkeys: {
+            volumeStep: 0.1,
+            seekStep: 5,
+            enableFull: true,
+            enableMute: true,
+            enableVolumeScroll: true,
+        },
+    },
+});
+
+
