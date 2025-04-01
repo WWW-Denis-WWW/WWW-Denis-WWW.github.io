9ed5dabac4edaf61eb36b7e907a426916e6bacf3
Denis <denistop54@mail.ru>
2025-04-01 14:25:15 +0500

    clear

--- 1/main.js
+++
@@ -1,8 +0,0 @@
-let token = '123'
-
-localStorage.setItem('token', token)
-
-console.log('main.js: ', localStorage)
-setTimeout(() => {
-    console.log('main.js: ', localStorage)
-}, 100)
--- 1/public/main2.js
+++
@@ -1,5 +0,0 @@
-localStorage.setItem('token2', '1221312ss')
-
-console.log('main2.js: ', localStorage)
-console.log('main2.js: ', localStorage.getItem('token'))
-console.log('main2.js: ', window.top.localStorage)
